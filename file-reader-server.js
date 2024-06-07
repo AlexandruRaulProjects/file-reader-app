92359038e70efe9cecfc023f4a1b35c9f88dd47b
Alexandru Jula <jula.alexandru11@yahoo.com>
2024-06-07 14:10:25 +0300

    Modified .env file

--- file-reader-server.js
+++ file-reader-server.js
@@ -16,13 +16,12 @@ const PORT = process.env.PORT;
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "audiobook-react-native.firebaseapp.com",
-  databaseURL:
-    "https://audiobook-react-native-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "audiobook-react-native",
-  storageBucket: "audiobook-react-native.appspot.com",
-  messagingSenderId: "768939966835",
-  appId: "1:768939966835:web:ac0457ccf78d1664c9c506",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_UR,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 console.log(PORT);
